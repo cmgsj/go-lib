6066e2ffc176f9ccd91e6ea754f7e9193ad5b5af
mike9107 <9107mgarcia@gmail.com>
2024-06-19 00:25:18 -0400

    add cli package

---
+++ cli/cli.go
@@ -0,0 +1,100 @@
+package cli
+
+import (
+	"os"
+	"time"
+)
+
+var DefaultFlagSet = NewFlagSet(os.Args[0], ExitOnError)
+
+func EnvPrefix() string {
+	return DefaultFlagSet.EnvPrefix()
+}
+
+func SetEnvPrefix(envPrefix string) {
+	DefaultFlagSet.SetEnvPrefix(envPrefix)
+}
+
+func Usage() {
+	DefaultFlagSet.Usage()
+}
+
+func SetUsage(usage func(*FlagSet)) {
+	DefaultFlagSet.SetUsage(usage)
+}
+
+func PrintDefaults() {
+	DefaultFlagSet.PrintDefaults()
+}
+
+func Parse() error {
+	return DefaultFlagSet.Parse(os.Args[1:])
+}
+
+func Parsed() bool {
+	return DefaultFlagSet.Parsed()
+}
+
+func Arg(i int) string {
+	return DefaultFlagSet.Arg(i)
+}
+
+func Args() []string {
+	return DefaultFlagSet.Args()
+}
+
+func NArg() int {
+	return DefaultFlagSet.NArg()
+}
+
+func Set(name, value string) error {
+	return DefaultFlagSet.Set(name, value)
+}
+
+func Lookup(name string) *FlagInfo {
+	return DefaultFlagSet.Lookup(name)
+}
+
+func NFlag() int {
+	return DefaultFlagSet.NFlag()
+}
+
+func Visit(fn func(*FlagInfo)) {
+	DefaultFlagSet.Visit(fn)
+}
+
+func VisitAll(fn func(*FlagInfo)) {
+	DefaultFlagSet.VisitAll(fn)
+}
+
+func StringFlag(name, usage string, required bool, defaults ...string) *Flag[string] {
+	return DefaultFlagSet.StringFlag(name, usage, required, defaults...)
+}
+
+func BoolFlag(name, usage string, required bool, defaults ...bool) *Flag[bool] {
+	return DefaultFlagSet.BoolFlag(name, usage, required, defaults...)
+}
+
+func IntFlag(name, usage string, required bool, defaults ...int) *Flag[int] {
+	return DefaultFlagSet.IntFlag(name, usage, required, defaults...)
+}
+
+func Int64Flag(name, usage string, required bool, defaults ...int64) *Flag[int64] {
+	return DefaultFlagSet.Int64Flag(name, usage, required, defaults...)
+}
+
+func UintFlag(name, usage string, required bool, defaults ...uint) *Flag[uint] {
+	return DefaultFlagSet.UintFlag(name, usage, required, defaults...)
+}
+
+func Uint64Flag(name, usage string, required bool, defaults ...uint64) *Flag[uint64] {
+	return DefaultFlagSet.Uint64Flag(name, usage, required, defaults...)
+}
+
+func Float64Flag(name, usage string, required bool, defaults ...float64) *Flag[float64] {
+	return DefaultFlagSet.Float64Flag(name, usage, required, defaults...)
+}
+
+func DurationFlag(name, usage string, required bool, defaults ...time.Duration) *Flag[time.Duration] {
+	return DefaultFlagSet.DurationFlag(name, usage, required, defaults...)
+}
---
+++ cli/flag_set.go
@@ -0,0 +1,126 @@
+package cli
+
+import (
+	"errors"
+	"flag"
+	"time"
+)
+
+type FlagSet struct {
+	flagSet   *flag.FlagSet
+	flags     []interface{ parse() error }
+	envPrefix string
+}
+
+func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet {
+	return &FlagSet{
+		flagSet: flag.NewFlagSet(name, errorHandling),
+	}
+}
+
+func (f *FlagSet) EnvPrefix() string {
+	return f.envPrefix
+}
+
+func (f *FlagSet) SetEnvPrefix(envPrefix string) {
+	f.envPrefix = envPrefix
+}
+
+func (f *FlagSet) Usage() {
+	f.flagSet.Usage()
+}
+
+func (f *FlagSet) SetUsage(usage func(*FlagSet)) {
+	f.flagSet.Usage = func() { usage(f) }
+}
+
+func (f *FlagSet) PrintDefaults() {
+	f.flagSet.PrintDefaults()
+}
+
+func (f *FlagSet) Parse(args []string) error {
+	return f.parse(args)
+}
+
+func (f *FlagSet) Parsed() bool {
+	return f.flagSet.Parsed()
+}
+
+func (f *FlagSet) Arg(i int) string {
+	return f.flagSet.Arg(i)
+}
+
+func (f *FlagSet) Args() []string {
+	return f.flagSet.Args()
+}
+
+func (f *FlagSet) NArg() int {
+	return f.flagSet.NArg()
+}
+
+func (f *FlagSet) Set(name, value string) error {
+	return f.flagSet.Set(name, value)
+}
+
+func (f *FlagSet) Lookup(name string) *FlagInfo {
+	return f.flagSet.Lookup(name)
+}
+
+func (f *FlagSet) NFlag() int {
+	return f.flagSet.NFlag()
+}
+
+func (f *FlagSet) Visit(fn func(*FlagInfo)) {
+	f.flagSet.Visit(fn)
+}
+
+func (f *FlagSet) VisitAll(fn func(*FlagInfo)) {
+	f.flagSet.VisitAll(fn)
+}
+
+func (f *FlagSet) StringFlag(name, usage string, required bool, defaults ...string) *Flag[string] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) BoolFlag(name, usage string, required bool, defaults ...bool) *Flag[bool] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) IntFlag(name, usage string, required bool, defaults ...int) *Flag[int] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) Int64Flag(name, usage string, required bool, defaults ...int64) *Flag[int64] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) UintFlag(name, usage string, required bool, defaults ...uint) *Flag[uint] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) Uint64Flag(name, usage string, required bool, defaults ...uint64) *Flag[uint64] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) Float64Flag(name, usage string, required bool, defaults ...float64) *Flag[float64] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) DurationFlag(name, usage string, required bool, defaults ...time.Duration) *Flag[time.Duration] {
+	return newFlag(f, name, usage, required, defaults...)
+}
+
+func (f *FlagSet) parse(args []string) error {
+	err := f.flagSet.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	var errs []error
+
+	for _, flag := range f.flags {
+		errs = append(errs, flag.parse())
+	}
+
+	return errors.Join(errs...)
+}
