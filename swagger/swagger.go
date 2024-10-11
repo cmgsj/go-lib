3e94aaf2070ada1c9b9593a7b5ca66cd95e99173
cmgsj <9107mgarcia@gmail.com>
2024-10-11 19:11:21 -0400

    add swagger pkg

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -1,4 +1,4 @@
-package openapi
+package swagger
 
 import (
 	"bytes"
@@ -12,9 +12,9 @@ import (
 
 var (
 	//go:embed all:docs
-	swaggerDocs embed.FS
+	docs embed.FS
 	//go:embed swagger-initializer.js
-	swaggerInit string
+	initializer string
 )
 
 type Schema struct {
@@ -22,8 +22,8 @@ type Schema struct {
 	Content []byte
 }
 
-func SwaggerDocs(prefix string, schemas ...Schema) http.Handler {
-	hanler, err := NewSwaggerDocsHandler(prefix, schemas...)
+func Docs(prefix string, schemas ...Schema) http.Handler {
+	hanler, err := NewDocsHandler(prefix, schemas...)
 	if err != nil {
 		panic(err)
 	}
@@ -31,7 +31,7 @@ func SwaggerDocs(prefix string, schemas ...Schema) http.Handler {
 	return hanler
 }
 
-func NewSwaggerDocsHandler(prefix string, schemas ...Schema) (http.Handler, error) {
+func NewDocsHandler(prefix string, schemas ...Schema) (http.Handler, error) {
 	prefix = strings.TrimSuffix(prefix, "/")
 	prefix = strings.TrimSuffix(prefix, "/*")
 
@@ -44,38 +44,38 @@ func NewSwaggerDocsHandler(prefix string, schemas ...Schema) (http.Handler, erro
 		initParams[schemaURL] = schema.Name
 	}
 
-	docsFS, err := fs.Sub(swaggerDocs, "docs")
+	docsFS, err := fs.Sub(docs, "docs")
 	if err != nil {
 		return nil, err
 	}
 
-	initTmpl, err := template.New("swagger-initializer").Parse(swaggerInit)
+	initializerTmpl, err := template.New("swagger-initializer").Parse(initializer)
 	if err != nil {
 		return nil, err
 	}
 
-	var initBuf bytes.Buffer
+	var initializerBuf bytes.Buffer
 
-	err = initTmpl.Execute(&initBuf, initParams)
+	err = initializerTmpl.Execute(&initializerBuf, initParams)
 	if err != nil {
 		return nil, err
 	}
 
 	initURL := fmt.Sprintf("%s/swagger-initializer.js", prefix)
-	overrides[initURL] = initBuf.Bytes()
+	overrides[initURL] = initializerBuf.Bytes()
 
-	return &swaggerDocsHandler{
+	return &docsHandler{
 		docs:      http.StripPrefix(prefix, http.FileServer(http.FS(docsFS))),
 		overrides: overrides,
 	}, nil
 }
 
-type swaggerDocsHandler struct {
+type docsHandler struct {
 	docs      http.Handler
 	overrides map[string][]byte
 }
 
-func (h *swaggerDocsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *docsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	content, ok := h.overrides[r.URL.Path]
 	if ok {
 		_, err := w.Write(content)
