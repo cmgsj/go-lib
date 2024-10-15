6fb1bc979e4167ac40a55762733b010470d3fd43
cmgsj <9107mgarcia@gmail.com>
2024-10-15 00:00:13 -0400

    fix future pkg

--- future/future.go
+++ future/future.go
@@ -3,62 +3,47 @@ package future
 import (
 	"context"
 	"errors"
-	"sync"
 )
 
 type Future[T any] interface {
-	Task[T]
-	Execute()
+	Get(ctx context.Context) (T, error)
 	Done() <-chan struct{}
 	IsDone() bool
 }
 
-func New[T any](ctx context.Context, task Task[T]) Future[T] {
-	return newFuture(ctx, task)
-}
-
-func Execute[T any](ctx context.Context, task Task[T]) Future[T] {
-	f := newFuture(ctx, task)
-	f.Execute()
-	return f
-}
-
-func newFuture[T any](ctx context.Context, task Task[T]) *future[T] {
+func NewFuture[T any](ctx context.Context, task func(ctx context.Context) (T, error)) Future[T] {
 	ctx, cancel := context.WithCancelCause(ctx)
-	return &future[T]{
-		ctx:    ctx,
-		cancel: cancel,
-		task:   task,
+
+	f := &future[T]{
+		ctx: ctx,
 	}
+
+	go func() {
+		f.result, f.err = task(f.ctx)
+		cancel(errDone)
+	}()
+
+	return f
 }
 
 var errDone = errors.New("done")
 
 type future[T any] struct {
-	once   sync.Once
 	ctx    context.Context
-	cancel context.CancelCauseFunc
-	task   Task[T]
 	result T
 	err    error
 }
 
-func (f *future[T]) Execute() {
-	go f.once.Do(func() {
-		f.result, f.err = f.task.Result(f.ctx)
-		f.cancel(errDone)
-	})
-}
-
-func (f *future[T]) Result(ctx context.Context) (T, error) {
-	f.Execute()
+func (f *future[T]) Get(ctx context.Context) (T, error) {
 	select {
 	case <-ctx.Done():
 		return f.result, ctx.Err()
+
 	case <-f.ctx.Done():
 		if context.Cause(f.ctx) == errDone {
 			return f.result, f.err
 		}
+
 		return f.result, f.ctx.Err()
 	}
 }
@@ -71,6 +56,7 @@ func (f *future[T]) IsDone() bool {
 	select {
 	case <-f.Done():
 		return true
+
 	default:
 		return false
 	}
